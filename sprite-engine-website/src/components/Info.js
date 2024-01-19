27d9c94458a66702b909970f8064054f49a836f3
jkatsanis <katsanis18@gmail.com>
2024-01-19 12:09:45 +0100

    uga

---
+++ sprite-engine-website/src/components/Info.js
@@ -0,0 +1,13 @@
+// src/components/Info.js
+import React from 'react';
+
+function Info() {
+  return (
+    <div>
+      <h2>Info Page</h2>
+      <p>This is the info page content.</p>
+    </div>
+  );
+}
+
+export default Info;
