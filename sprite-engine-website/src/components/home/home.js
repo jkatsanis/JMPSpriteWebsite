00e14142ef7e48723eb67fa1b543f42ff7b0318e
jkatsanis <katsanis18@gmail.com>
2024-02-08 14:32:40 +0100

    progress

--- sprite-engine-website/src/components/home/home.js
+++ sprite-engine-website/src/components/home/home.js
@@ -9,6 +9,8 @@ function Home() {
 
   let website = new Link("ressources/icons/github.png", "Github", "https://github.com/jkatsanis");
   let engine = new Link("ressources/icons/github.png", "Github", "https://github.com/jkatsanis/SpriteEngineUI");
+  let youtube = new Link("ressources/icons/youtube.png", "Youtube", "https://www.youtube.com/channel/UCWihpyidnoBJ4G-V2TT6bVw")
+
   const myLeftValue = 100;
 
   return (
@@ -21,17 +23,14 @@ function Home() {
         <div className="video-text">The C++ <br /> 2D Game Engine <br /> Of The Future.</div>
       </div>
 
-
-
-
       <div style={{ height: '5rem' }}></div>
         <div className="centered-div">
           <h1>Get started now</h1>
 
+          <br/>
           <div className="separator"></div>
+          <br/>
 
-
-          <div style={{ height: '1rem' }}></div>
 
 
           <div className="like-windows-container">
@@ -42,10 +41,16 @@ function Home() {
               lin={website}/>
 
             <LikeWindow
-                imageSrc="ressources/pictures/githubcoverEngine.png"
-                title="Github-GameEngine"
-                description="This Project is a simple 2D Game Engine designed to be fully understand by one Person. It is written in pure C++, with the support of SFML & Imgui"
-                lin={engine}/>
+              imageSrc="ressources/pictures/githubcoverEngine.png"
+              title="Github-GameEngine"
+              description="This Project is a simple 2D Game Engine designed to be fully understand by one Person. It is written in pure C++, with the support of SFML & Imgui"
+              lin={engine}/>
+
+            <LikeWindow
+              imageSrc="ressources/pictures/youtubecover.png"
+              title="Youtube"
+              description="Feel free to watch my youtube videos, I introduce the gameengine, introduce new functions and create devlogs."
+              lin={youtube}/>
           </div>
 
       </div>
