56a957a6569ff49a59d1618acc0d287436ca68c0
jkatsanis <katsanis18@gmail.com>
2024-02-08 11:08:48 +0100

    progress

--- sprite-engine-website/src/components/home/home.js
+++ sprite-engine-website/src/components/home/home.js
@@ -3,13 +3,22 @@ import './home.css'; // Import CSS file for styling
 
 function Home() {
   return (
+    <div>
       <div className="video-container">
         <video className="responsive-video" autoPlay loop muted>
           <source src="ressources/videos/intro.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="video-text">The C++  <br/> 2D Game Engine <br/> Of The Future.   </div>
+        <div className="video-text">The C++  <br/> 2D Game Engine <br/> Of The Future. 
+        </div>
       </div>
+ 
+      <div className="general-info">
+        uga uga
+      </div>  
+
+    </div>
+
   );
 }
 
