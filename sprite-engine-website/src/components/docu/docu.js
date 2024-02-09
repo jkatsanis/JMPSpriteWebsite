5ebff9c4acc1b928b08d1145281103bd0efb79c0
Paul Achleitner <p.achleitner@students.htl-leonding.ac.at>
2024-02-09 12:10:59 +0100

    started threads

--- sprite-engine-website/src/components/docu/docu.js
+++ sprite-engine-website/src/components/docu/docu.js
@@ -3,7 +3,7 @@ import './docu.css';
 
 export function Docu(){
     return(
-        <div className="mainBackDocu">
+        <div className="mainBack">
             <h1>Welcome to the Documentation Page</h1>
             <p>Hello, I am new. This is a simple documentation page for the SpriteEngine project.</p>
             <p>Here, you can find various guides and references to help you understand and use SpriteEngine.</p>
--- sprite-engine-website/src/components/topbar/topbar.js
+++ sprite-engine-website/src/components/topbar/topbar.js
@@ -4,9 +4,10 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 import Home from 'components/home/home'
 import Info from 'components/info'
+import {Docu} from "components/docu/docu";
+import {Threads} from "components/threads/threads";
 
 import './topbar.css';
-import {Docu} from "components/docu/docu";
 
 function TopBar() {
   return (
@@ -18,6 +19,7 @@ function TopBar() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/info">Info</Link></li>
               <li><Link to="/docu">Docu</Link></li>
+                <li><Link to="/threads">Threads</Link></li>
           </ul>
         </nav>
       </div>
@@ -25,6 +27,7 @@ function TopBar() {
         <Route path="/" exact component={Home} />
         <Route path="/info" component={Info} />
           <Route path="/docu" component={Docu}/>
+            <Route path="/threads" component={Threads}/>
       </Switch>
     </div>
   );
