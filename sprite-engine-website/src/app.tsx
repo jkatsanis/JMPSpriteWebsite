9720c57c7161f1799bb524c8e145a9e2f3c21ba6
jkatsanis <katsanis18@gmail.com>
2024-02-12 15:43:15 +0100

    ui progressssss

--- sprite-engine-website/src/app.tsx
+++ sprite-engine-website/src/app.tsx
@@ -1,41 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import TopBar from './components/topbar/topbar';
-import Home from './components/home/home';
-import Info from './components/info';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/home/home";
+import Info from "./components/info";
+
 
 const App: React.FC = () => {
-  const [bgColor, setBgColor] = useState<string>('#1E1E1E');
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const colorIndex = Math.floor(scrollPosition / 10); // 160 is approximately 10rem in pixels
-
-      // Define your color palette here
-      const colors = ['#1E1E1E', '#2C3E50', '#34495E', '#16A085', '#27AE60'];
-
-      // Set background color based on colorIndex
-      setBgColor(colors[colorIndex]);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <TopBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/info" element={<Info />} />
-      </Routes>
-    
-    </BrowserRouter>
-  );
-};
+    return (      
+        <BrowserRouter>
+            <TopBar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/info" element={<Info/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App;
--- sprite-engine-website/src/components/bottom/bottom.tsx
+++ sprite-engine-website/src/components/bottom/bottom.tsx
@@ -15,13 +15,13 @@ export default class Bottom extends React.Component<BottomProps>
     {
         let engine = new Link("ressources/icons/github.png", "Github", "https://github.com/jkatsanis/SpriteEngineUI");
         let youtube = new Link("ressources/icons/youtube.png", "Youtube", "https://www.youtube.com/channel/UCWihpyidnoBJ4G-V2TT6bVw")
-        let mit = new Link(null, "MIT License", "https://www.youtube.com/channel/UCWihpyidnoBJ4G-V2TT6bVw")
+        let mit = new Link(null, "MIT License", "https://github.com/jkatsanis/SpriteEngineUI?tab=MIT-1-ov-file")
 
         return(
             <div className="bottom centered-div">
                 <div style={{ transform: "scale(0.7)" }}>
                     <div className="inline links">
-                        <CreateLink Link={engine} left={0}></CreateLink>
+                        <CreateLink Link={engine} left={10}></CreateLink>
                         <CreateLink Link={youtube} left={10}></CreateLink>
 
                     </div>
