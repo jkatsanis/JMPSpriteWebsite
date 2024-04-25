99402398f12fb81a02afbb2f70a13296d5c16e09
jkatsanis <katsanis18@gmail.com>
2024-04-25 14:45:25 +0200

    MAIN: Fatal changes

---
+++ sprite-engine-website/src/backend/server.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express from "express";
+import {accountRouter} from "./routes/accountRouter";
+import {threadRouter} from "./routes/threadRouter";
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use(express.static("build"));
+
+server.use("/api/accounts", accountRouter);
+server.use("/api/accounts", threadRouter);
+
+server.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
---
+++ sprite-engine-website/src/frontend/macros.tsx
@@ -0,0 +1,2 @@
+export const PATH_TO_ACCOUNT_FOLDER = "http://localhost:3000//ressources/";
+export const PATH_TO_RESSOURCES = "http://localhost:3000//ressources"
